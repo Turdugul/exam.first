4e35dd43fdb4307f7215fc1c98574d915cb664bf
Turdugul <okonbaevaturdugul@gmail.com>
2021-12-12 00:04:49 +0600

    fc

---
+++ app/src/main/java/com/example/myexam/Fragment2.kt
@@ -0,0 +1,6 @@
+package com.example.myexam
+
+import androidx.fragment.app.Fragment
+
+open class Fragment2 : Fragment(R.layout.fragment2) {
+}
---
+++ app/src/main/java/com/example/myexam/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.myexam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.about -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, Fragment1() )
+                    .commit()
+            }
+            R.id.classes -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, Fragment2() )
+                    .commit()
+            }
+            R.id.students -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, Fragment3() )
+                    .commit()
+            }
+            else -> {}
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
